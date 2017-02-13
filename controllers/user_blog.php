b7f5702b3975b21708d54a3f5299a877cdefa30e
sergey filipovich <psiloscop@gmail.com>
2017-02-13 09:34:35 +0600

    Using mktime

--- controllers/user_blog.php
+++ controllers/user_blog.php
@@ -137,8 +137,8 @@ class BLOGS_CTRL_UserBlog extends OW_ActionController
             $month = $arr[0];
             $year = $arr[1];
 
-            $lb = strtotime($year . '-' . $month . '-01 00:00:00');
-            $ub = strtotime($year . '-' . $month . '-01 23:59:59 +1 month -1 day');
+            $lb = mktime(null, null, null, $month, 1, $year);
+            $ub = mktime(-1, 59, 59, $month + 1, 1, $year);
 
             $list = $service->findUserPostListByPeriod($author->getId(), $lb, $ub, $first, $count);
 
